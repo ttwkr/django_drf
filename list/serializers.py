cac5da5020cb15f1d4672318bbd76b201e31376e
서상진 <seosangjin@seosangjin-ui-MacBookPro.local>
2020-06-20 11:20:15 +0900

    wip

--- list/serializers.py
+++ list/serializers.py
@@ -7,7 +7,7 @@ import json
 class TodolistSerializer(serializers.ModelSerializer):
     id = models.AutoField(primary_key=True)
     contents = serializers.CharField(required=False)
-    image_list = serializers.CharField()
+    image_list = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     deleted_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
@@ -17,8 +17,5 @@ class TodolistSerializer(serializers.ModelSerializer):
         fields = ('id', 'contents', 'image_list',
                   'created_at', 'updated_at', 'deleted_at',)
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['image_list'] = json.loads(instance.image_list)
-
-        return representation
+    def get_image_list(self, obj):
+        return json.loads(obj.image_list)
